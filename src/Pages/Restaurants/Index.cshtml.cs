a9836b349da79fbed2b9229a548f7646c6ad7ec5
Duc Vo <dvo4@seattleu.edu>
2022-10-16 10:54:49 -0700

    Created Razor page model Pages/Restaurants/Index.cshtml for restaurant index listing view

---
+++ src/Pages/Restaurants/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages.Restaurants
+{
+    /// <summary>
+    /// Restaurant Index Page Model
+    /// </summary>
+    public class IndexModel : PageModel
+    {
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="restaurantService"></param>
+        public IndexModel(JsonFileRestaurantService restaurantService)
+        {
+            RestaurantService = restaurantService;
+        }
+        // Data middletier
+        public JsonFileRestaurantService RestaurantService { get; }
+        
+        // The data to show
+        public IEnumerable<RestaurantModel> Restaurants { get; private set; }
+
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        public void OnGet()
+        {
+            Restaurants = RestaurantService.GetRestaurants();
+        }
+    }
+}
