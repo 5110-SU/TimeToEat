40ce1c77e0b66388ada58518242ce5b3a885dee1
sliu1SU <sliu1@seattleu.edu>
2022-10-18 01:18:38 -0700

    Added class ReadModel to Detail.cshtml.cs

--- src/Pages/Restaurants/Detail.cshtml.cs
+++ src/Pages/Restaurants/Detail.cshtml.cs
@@ -1,12 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ContosoCrafts.WebSite.Pages.Restaurants
+namespace ContosoCrafts.WebSite.Pages
 {
-    public class DetailModel : PageModel
+    public class ReadModel : PageModel
     {
-        public void OnGet()
+        // Data middletier
+        public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
+        public ReadModel(JsonFileProductService productService)
         {
+            ProductService = productService;
+        }
+
+        // The data to show
+        public ProductModel Product;
+
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        /// <param name="id"></param>
+        public void OnGet(string id)
+        {
+            IEnumerable<ProductModel> Products = ProductService.GetProducts();
+            Product = Products.FirstOrDefault(m => m.Id.Equals(id));
         }
     }
 }
