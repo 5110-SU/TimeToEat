7504e91b372e0508cb139bba36728778b1b5395a
sliu1SU <sliu1@seattleu.edu>
2022-10-18 11:13:46 -0700

    Updated 'OnGet' function in Detail.cshtml.cs

--- src/Pages/Restaurants/Detail.cshtml.cs
+++ src/Pages/Restaurants/Detail.cshtml.cs
@@ -1,38 +1,30 @@
-using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace ContosoCrafts.WebSite.Pages
+namespace ContosoCrafts.WebSite.Pages.Restaurants
 {
     public class ReadModel : PageModel
     {
         // Data middletier
         public JsonFileProductService ProductService { get; }
-
         /// <summary>
-        /// Defualt Construtor
+        /// Default Construtor
         /// </summary>
-        /// <param name="logger"></param>
         /// <param name="productService"></param>
         public ReadModel(JsonFileProductService productService)
         {
             ProductService = productService;
         }
-
         // The data to show
         public ProductModel Product;
-
         /// <summary>
         /// REST Get request
         /// </summary>
         /// <param name="id"></param>
         public void OnGet(string id)
         {
-            IEnumerable<ProductModel> Products = ProductService.GetProducts();
-            Product = Products.FirstOrDefault(m => m.Id.Equals(id));
+            Product = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
         }
     }
 }
