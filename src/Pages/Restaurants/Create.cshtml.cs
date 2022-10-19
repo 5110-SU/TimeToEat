129af42cc23cc711b5caab99cc1759ea6c3a34f8
Emily Lee <87784443+emily-bytes@users.noreply.github.com>
2022-10-18 22:24:58 -0700

    Implemented basic details of Restaurants/Create.cshtml.cs

---
+++ src/Pages/Restaurants/Create.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+namespace ContosoCrafts.WebSite.Pages.Restaurants
+{
+    public class CreateModel : PageModel
+    {
+        // Data middletier
+        public JsonFileProductService ProductService { get; }
+        /// <summary>
+        /// Default Construtor
+        /// </summary>
+        /// <param name="productService"></param>
+        public CreateModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+        // The data to show
+        public ProductModel Product;
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        /// <param name="id"></param>
+        public void OnGet(string id)
+        {
+            Product = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
+        }
+    }
+}
