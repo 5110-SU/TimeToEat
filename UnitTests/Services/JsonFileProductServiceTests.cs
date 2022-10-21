499380e50f015a3954d5643c22d95e6ba34244ef
sliu1SU <sliu1@seattleu.edu>
2022-10-20 17:50:19 -0700

    Implemented unit test for addRating function in JsonFileProductServiceTests.cs

--- UnitTests/Services/JsonFileProductServiceTests.cs
+++ UnitTests/Services/JsonFileProductServiceTests.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 
 using ContosoCrafts.WebSite.Models;
+using NUnit.Framework.Internal;
 
 namespace UnitTests.Pages.Product.AddRating
 {
@@ -33,36 +34,102 @@ namespace UnitTests.Pages.Product.AddRating
         }
 
         [Test]
-        public void AddRating_InValid_()
+        public void AddRating_InValid_Product_Empty_Should_Return_False()
         {
             // Arrange
 
             // Act
-            //var result = TestHelper.ProductService.AddRating(null, 1);
+            var result = TestHelper.ProductService.AddRating("", 1);
 
             // Assert
-            //Assert.AreEqual(false, result);
+            Assert.AreEqual(false, result);
         }
 
-        // ....
-
         [Test]
-        public void AddRating_Valid_Product_Valid_Rating_Valid_Should_Return_True()
+        public void AddRating_Valid_Product_Rating_5_Should_Return_True()
         {
             // Arrange
 
             // Get the First data item
-            var data = TestHelper.ProductService.GetAllData().First();
+            var testID = "Steak House";
+            var data = TestHelper.ProductService.GetAllData().FirstOrDefault(x => x.Id.Equals(testID));
             var countOriginal = data.Ratings.Length;
 
             // Act
             var result = TestHelper.ProductService.AddRating(data.Id, 5);
-            var dataNewList = TestHelper.ProductService.GetAllData().First();
+            var dataNewList = TestHelper.ProductService.GetAllData().FirstOrDefault(x => x.Id.Equals(testID));
 
             // Assert
             Assert.AreEqual(true, result);
             Assert.AreEqual(countOriginal + 1, dataNewList.Ratings.Length);
             Assert.AreEqual(5, dataNewList.Ratings.Last());
+
+            // reset
+            var toReset = TestHelper.ProductService.GetAllData().FirstOrDefault(x => x.Id.Equals(testID));
+            var ratings = toReset.Ratings.ToList();
+            ratings.RemoveAt(toReset.Ratings.Length - 1);
+            toReset.Ratings = ratings.ToArray();
+        }
+
+        [Test]
+        public void AddRating_InValid_Product_InvalidID_Should_Return_False()
+        {
+            // assign
+
+            // act
+            bool result = TestHelper.ProductService.AddRating("sdasdasd", 2);
+
+            // assert
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void AddRating_InValid_Product_Rating_Low_Should_Return_False()
+        {
+            // assign
+
+            // act
+            ProductModel data = TestHelper.ProductService.GetAllData().First();
+            bool result = TestHelper.ProductService.AddRating(data.Id, -8);
+
+            // assert
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void AddRating_InValid_Product_Rating_High_Should_Return_False()
+        {
+            // assign
+
+            // act
+            ProductModel data = TestHelper.ProductService.GetAllData().First();
+            bool result = TestHelper.ProductService.AddRating(data.Id, 8);
+
+            // assert
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void AddRating_Valid_Product_Empty_Rating_Should_Return_True()
+        {
+            // assign
+
+            // Get the First data item
+            string testID = "Vietnamese";
+            ProductModel data = TestHelper.ProductService.GetAllData().FirstOrDefault(x => x.Id.Equals(testID));
+
+            // act
+            bool result = TestHelper.ProductService.AddRating(testID, 5);
+            ProductModel dataNewList = TestHelper.ProductService.GetAllData().FirstOrDefault(x => x.Id.Equals(testID));
+
+            // assert
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, dataNewList.Ratings.Length);
+            Assert.AreEqual(5, dataNewList.Ratings.Last());
+
+            // reset
+            ProductModel toReset = TestHelper.ProductService.GetAllData().FirstOrDefault(x => x.Id.Equals(testID));
+            toReset.Ratings = null;
         }
         #endregion AddRating
 
