4fad5655a33520d42de8a9a71428da33660f0c81
Duc Vo <dvo4@seattleu.edu>
2022-10-23 21:47:07 -0700

    Added tests for OnGet method on ReadModel on Detail.cshtml.Tests.cs

---
+++ UnitTests/Pages/Restaurants/Detail.cshtml.Tests.cs
@@ -0,0 +1,53 @@
+﻿using ContosoCrafts.WebSite.Pages.Restaurants;
+using NUnit.Framework;
+using System.Linq;
+
+namespace UnitTests.Pages.Restaurants
+{
+    public class DetailTests
+    {
+        #region TestSetup
+
+        public static ReadModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new ReadModel(TestHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_With_New_Product_ID_Should_Retrieve_Null()
+        {
+            // Arrange
+            var Id = System.Guid.NewGuid().ToString();
+            
+            // Act
+            pageModel.OnGet(Id);
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(null, pageModel.Product);
+        }
+
+        [Test]
+        public void OnGet_Valid_With_Existing_Product_ID_Should_Retrieve_Expected_Product()
+        {
+            // Arrange
+            var Product = TestHelper.ProductService.GetProducts().First();
+
+            // Act
+            pageModel.OnGet(Product.Id);
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(Product.Id, pageModel.Product.Id);
+        }
+        #endregion OnGet
+    }
+}
