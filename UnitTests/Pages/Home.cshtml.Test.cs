06b383ec6ebd9381123e8ac81dcddffc522b1bcd
Hong Yang <hongyue98@hotmail.com>
2022-10-23 22:33:26 -0700

    added test for onget method in home.cshtml.test.cs

--- UnitTests/Pages/Home.cshtml.Test.cs
+++ UnitTests/Pages/Home.cshtml.Test.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
-namespace UnitTests.Pages
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages;
+
+namespace UnitTests.Pages.Home
 {
-    internal class Home
+    public class HomeTests
     {
+        #region TestSetup
+
+        public static HomeModel pageModel;
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new HomeModel()
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Home_Page()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+        #endregion OnGet
     }
 }
