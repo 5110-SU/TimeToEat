7ca327105e91f5190e22b5059a4faf3c5da62a7c
Duc Vo <dvo4@seattleu.edu>
2022-10-25 15:39:26 -0700

    Created OnPost action to update existing item in data storage in Update.cshtml.cs

--- src/Pages/Restaurants/Update.cshtml.cs
+++ src/Pages/Restaurants/Update.cshtml.cs
@@ -1,13 +1,19 @@
-﻿using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
+using ContosoCrafts.WebSite.Models;
+
 namespace ContosoCrafts.WebSite.Pages.Restaurants
 {
     public class UpdateModel : PageModel
     {
         // Data middletier
         public JsonFileProductService ProductService { get; }
+
+        // Bind the data for the form
+        [BindProperty]
+        public ProductModel Product { get; set; }
+
         /// <summary>
         /// Default Construtor
         /// </summary>
@@ -16,15 +22,37 @@ namespace ContosoCrafts.WebSite.Pages.Restaurants
         {
             ProductService = productService;
         }
-        // The data to show
-        public ProductModel Product;
+        
         /// <summary>
         /// REST Get request
         /// </summary>
-        /// <param name="id"></param>
         public void OnGet(string id)
         {
-            Product = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
+            // Get the product from the data storage
+            Product = ProductService.GetProduct(id);
+        }
+
+        /// <summary>
+        /// REST Post request to Update existing product
+        /// </summary>
+        /// <returns>Detail Page</returns>
+        public IActionResult OnPost()
+        {
+            // Return to the page if the input data is not valid
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Save updated product into database
+            ProductModel product = ProductService.UpdateData(Product);
+
+            if (product == null) {
+                return RedirectToPage("./NotFound");
+            }
+            
+            // Redirect user to the Updated restaurant detail page
+            return RedirectToPage("./Detail", new {id = product.Id});
         }
     }
 }
