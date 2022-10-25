a9ab646d0b074f7e2266a05b609f81d899f0ef5a
Emily Lee <87784443+emily-bytes@users.noreply.github.com>
2022-10-24 19:58:46 -0700

    Added Create.cshtml.Tests.cs page to the project.

---
+++ UnitTests/Pages/Create.cshtml.Tests.cs
@@ -0,0 +1,14 @@
+﻿using ContosoCrafts.WebSite.Pages.Restaurants;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Pages.Create
+{
+    public class CreateTests
+    {
+    }
+}
