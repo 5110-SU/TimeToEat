0c5e81ef98f6ac1e2d023df132bfc2ed7ca704c0
hyang98 <hongyue98@hotmail.com>
2022-10-25 16:31:00 -0700

    added unit test for update in update.cshtml.tests.cs

---
+++ UnitTests/Pages/Restaurants/Update.cshtml.Tests.cs
@@ -0,0 +1,39 @@
+﻿using ContosoCrafts.WebSite.Pages.Restaurants;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Pages.Create
+{
+    public class UpdateTests
+    {
+        #region TestSetup
+        public static UpdateModel pageModel;
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new UpdateModel(TestHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Create_Page()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet("");
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+        #endregion OnGet
+    }
+}
