f6db8e4e5d28b94be26d28df1dd6c569f983ba38
sliu1SU <sliu1@seattleu.edu>
2022-10-25 19:14:01 -0700

    Added OnPost_Valid_Image_Return_Create_Page() to Create.cshtml.Tests.cs

--- UnitTests/Pages/Restaurants/Create.cshtml.Tests.cs
+++ UnitTests/Pages/Restaurants/Create.cshtml.Tests.cs
@@ -1,4 +1,5 @@
 ﻿using ContosoCrafts.WebSite.Pages.Restaurants;
+using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -35,5 +36,24 @@ namespace UnitTests.Pages.Create
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
         }
         #endregion OnGet
+
+        #region OnPost
+        [Test]
+        public void OnPost_Valid_Image_Return_Create_Page()
+        {
+            // assign
+            var defImg = "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/restaurant-instagram-post-advertisement-design-template-5e3dde31601916fac13b611b18066f52_screen.jpg?ts=1622274831";
+
+            // act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+
+            // reset
+
+
+        }
+        #endregion OnPost
     }
 }
