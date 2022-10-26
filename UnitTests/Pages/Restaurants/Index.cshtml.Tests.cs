d032340f57b7e98bad5878c5983d71ecc9e08916
Duc Vo <dvo4@seattleu.edu>
2022-10-25 17:00:43 -0700

    Added unit test for restaurants Index Page in Index.cshtml.cs

---
+++ UnitTests/Pages/Restaurants/Index.cshtml.Tests.cs
@@ -0,0 +1,38 @@
+using ContosoCrafts.WebSite.Pages.Restaurants;
+using NUnit.Framework;
+using System.Linq;
+
+namespace UnitTests.Pages.Restaurants
+{
+    public class IndexTests
+    {
+        #region TestSetup
+
+        public static IndexModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new IndexModel(TestHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Should_Retrieve_All_Products()
+        {
+            // Arrange
+            var data = TestHelper.ProductService.GetAllData().Count();
+            // Act
+            pageModel.OnGet();
+      
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(data, pageModel.Products.Count());
+        }
+        #endregion OnGet
+    }
+}
