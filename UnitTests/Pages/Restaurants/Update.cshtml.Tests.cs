8228582ee0b7c51472f12fb33a78a2cdb2f864fd
Duc Vo <dvo4@seattleu.edu>
2022-10-25 17:32:35 -0700

    Added Unit Tests for OnPost method for restaurants update page in Update.cshtml.Test.cs

--- UnitTests/Pages/Restaurants/Update.cshtml.Tests.cs
+++ UnitTests/Pages/Restaurants/Update.cshtml.Tests.cs
@@ -1,10 +1,8 @@
 ﻿using ContosoCrafts.WebSite.Pages.Restaurants;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UnitTests.Pages.Create
 {
@@ -35,5 +33,57 @@ namespace UnitTests.Pages.Create
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
         }
         #endregion OnGet
+
+        #region OnPost
+        [Test]
+        public void OnPost_Valid_Should_Redirect_To_Detail_Page()
+        {
+            // Arrange
+            var data = TestHelper.ProductService.GetProducts().First();
+            pageModel.Product = data;
+
+            // Act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, result.PageName.Contains("Detail"));
+        }
+
+        [Test]
+        public void OnPost_Invalid_Should_Return_Same_Page()
+        {
+            // Arrange
+            var data = TestHelper.ProductService.GetProducts().First();
+            data.Title = "";
+            pageModel.Product = data;
+
+            // Force an invalid error state
+            pageModel.ModelState.AddModelError("title-err", "title required");
+
+            // Act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+            
+            // Assert
+            Assert.AreEqual(false, pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, result.PageName.Contains("Update"));
+        }
+
+        [Test]
+        public void OnPost_Invalid_Should_Return_Redirect_Index_Page()
+        {
+            // Arrange
+            var data = TestHelper.ProductService.GetProducts().First();
+            data.Id = "not-found-id";
+            pageModel.Product = data;
+
+            // Act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, result.PageName.Contains("Index"));
+        }
+        #endregion OnPost
     }
 }
