8943b1c2c6cfdbc7b346ad5f2d5ec1012447c256
Duc Vo <dvo4@seattleu.edu>
2022-10-27 11:14:07 -0700

    Added comments in ProductsController.cs

--- src/Controllers/ProductsController.cs
+++ src/Controllers/ProductsController.cs
@@ -7,17 +7,29 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Controllers
 {
+    /// <summary>
+    /// Controller class for the products API endpoints.
+    /// </summary>
     [Route("api/restaurants")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        /// <summary>
+        /// Contructor for ProductsController with service dependencie injection.
+        /// </summary>
+        /// <param name="productsService">The products data service</param>
         public ProductsController(JsonFileProductService productService)
         {
             ProductService = productService;
         }
 
+        // Data middle tier 
         public JsonFileProductService ProductService { get; }
-
+        
+        /// <summary>
+        /// Get all products from controller base endpoints.
+        /// </summary>
+        /// <returns>HTTP 200 OK with JSON products in body</returns>
         [HttpGet]
         public IEnumerable<ProductModel> Get()
         {
