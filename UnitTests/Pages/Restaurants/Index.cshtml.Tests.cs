a38427267c9313f7da2b5e95ec80edabc244eb02
Emily Lee <87784443+emily-bytes@users.noreply.github.com>
2022-10-31 12:13:38 -0700

    Added comment for IndexTests class in Index.cshtml.Tests.cs

--- UnitTests/Pages/Restaurants/Index.cshtml.Tests.cs
+++ UnitTests/Pages/Restaurants/Index.cshtml.Tests.cs
@@ -4,6 +4,9 @@ using System.Linq;
 
 namespace UnitTests.Pages.Restaurants
 {
+    /// <summary>
+    /// Unit tests for the IndexModel class
+    /// </summary>
     public class IndexTests
     {
         #region TestSetup
