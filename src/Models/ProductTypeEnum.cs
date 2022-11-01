5ff9bd7c9c7827d57d42aea6b75193d4d6c6d7a0
Duc Vo <dvo4@seattleu.edu>
2022-11-01 09:34:25 -0700

    Added comments to ProductTypeEnum.cs

--- src/Models/ProductTypeEnum.cs
+++ src/Models/ProductTypeEnum.cs
@@ -5,25 +5,34 @@ using System.Threading.Tasks;
 
 namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// Product Types Enumeration
+    /// </summary>
     public enum ProductTypeEnum
     {
-        Undefined = 0,
-        Amature = 1,
-        Antique = 5,
-        Collectable = 130,
-        Commercial = 55,
+        Undefined = 0,  // Default value
+        Fastfood = 1,  // Fastfood Restaurant
+        Cafe = 5,  // Cafe Restaurant
+        BBQ = 130,  // BBQ Restaurant
+        FineDining = 55,  // Fine Dining Restaurant
     }
 
+    /// <summary>
+    /// Product Type Functionalities 
+    /// </summary>
     public static class ProductTypeEnumExtensions
-    {
+    {   
+        /// <summary>
+        /// Get Product Type Name from Product Type enum
+        /// </summary>
         public static string DisplayName(this ProductTypeEnum data)
         {
             return data switch
             {
-                ProductTypeEnum.Amature => "Hand Made Items",
-                ProductTypeEnum.Antique => "Antiques",
-                ProductTypeEnum.Collectable => "Collectables",
-                ProductTypeEnum.Commercial => "Commercial goods",
+                ProductTypeEnum.Fastfood => "Fastfood",
+                ProductTypeEnum.Cafe => "Cafe",
+                ProductTypeEnum.BBQ => "BBQ",
+                ProductTypeEnum.FineDining => "FineDining",
  
                 // Default, Unknown
                 _ => "",
