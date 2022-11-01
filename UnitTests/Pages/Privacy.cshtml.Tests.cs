d92114f845be6eccf7651ba9188acd45d5d430b4
hyang98 <hongyue98@hotmail.com>
2022-10-31 19:13:39 -0700

    added comments to privacy.cshtml.test.cs

--- UnitTests/Pages/Privacy.cshtml.Tests.cs
+++ UnitTests/Pages/Privacy.cshtml.Tests.cs
@@ -8,11 +8,19 @@ using ContosoCrafts.WebSite.Pages;
 
 namespace UnitTests.Pages.Privacy
 {
+    /// <summary>
+    /// Unit testing Privacy class
+    /// Testing methods:
+    ///     - OnGet()
+    /// </summary>
     public class PrivacyTests
     {
         #region TestSetup
         public static PrivacyModel pageModel;
 
+        /// <summary>
+        /// Setting up the test environment
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -27,6 +35,9 @@ namespace UnitTests.Pages.Privacy
 
         #endregion TestSetup
 
+        /// <summary>
+        /// ModelState.IsValid should return true after OnGet() is called
+        /// </summary>
         #region OnGet
         [Test]
         public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
