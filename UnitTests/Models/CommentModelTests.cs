5421a6818bda7335e2ce36211e32e58bd5eee59b
sliu1SU <sliu1@seattleu.edu>
2022-11-01 14:01:19 -0700

    added TestSetup to CommentModelTests.cs

--- UnitTests/Models/CommentModelTests.cs
+++ UnitTests/Models/CommentModelTests.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Pages;
+using NUnit.Framework;
 
 namespace UnitTests.Models
 {
     internal class CommentModelTests
     {
+        #region TestSetup
+
+        /// Comment model instace to test 
+        public static CommentModel TestModel;
+
+        /// <summary>
+        /// Set up the test environmenmt
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            TestModel = new CommentModel()
+            {
+
+            };
+        }
+
+        #endregion TestSetup
     }
 }
