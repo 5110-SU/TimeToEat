d710ca8de68ca168419494190043ff3bf0e73f65
sliu1SU <sliu1@seattleu.edu>
2022-11-01 14:45:42 -0700

    added TestSetup to ProductTypeEnumTests.cs

--- UnitTests/Models/ProductTypeEnumTests.cs
+++ UnitTests/Models/ProductTypeEnumTests.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Pages.Restaurants;
+using NUnit.Framework;
 
 namespace UnitTests.Models
 {
     internal class ProductTypeEnumTests
     {
+        #region TestSetup
+
+        // The delete page model to test
+        public static ProductTypeEnum TestModel;
+
+        /// <summary>
+        /// Setup the test environment for the TestModel
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            TestModel = new ProductTypeEnum()
+            {
+
+            };
+        }
+
+        #endregion TestSetup
     }
 }
