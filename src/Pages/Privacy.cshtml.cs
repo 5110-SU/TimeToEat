4c8d0b346f7ed9f23f749e3e992efc8d4a4f7085
hyang98 <hongyue98@hotmail.com>
2022-10-31 19:26:03 -0700

    added comments to privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -3,10 +3,21 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Allows attributes for razor page 
+    /// </summary>
     public class PrivacyModel : PageModel
     {
+        // The following methods are simply implementations of 
+        // C#http calls to our logger, productservice so that we can display to page for viewing
+
         private readonly ILogger<PrivacyModel> _logger;
 
+        /// <summary>
+        /// Creates a logger, ILogger<PrivacyModel>, 
+        /// which uses a log category of the fully qualified name of the type PrivacyModel
+        /// </summary>
+     
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
