8c3a9353210499941fbbc130aa6683850375575c
Hong Yang <hongyue98@hotmail.com>
2022-11-01 22:35:14 -0700

    updated comments for home page in home.cshtml.cs

--- src/Pages/Home.cshtml.cs
+++ src/Pages/Home.cshtml.cs
@@ -3,8 +3,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Unit test for Home model page
+    /// </summary>
     public class HomeModel : PageModel
     {
+        /// <summary>
+        /// When a user makes a GET request to a page, we invoke this method
+        /// </summary>
         public void OnGet()
         {
         }
