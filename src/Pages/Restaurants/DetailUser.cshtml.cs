e18a9d7a38240ae8b8c2f721604878c6ab39e9ff
sliu1SU <sliu1@seattleu.edu>
2022-11-07 10:12:59 -0800

    implemented DetailUserModel class in DetailUser.cshtml.cs

--- src/Pages/Restaurants/DetailUser.cshtml.cs
+++ src/Pages/Restaurants/DetailUser.cshtml.cs
@@ -1,12 +1,33 @@
-using Microsoft.AspNetCore.Mvc;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages.Restaurants
 {
     public class DetailUserModel : PageModel
     {
-        public void OnGet()
+        // Data middletier
+        public JsonFileProductService ProductService { get; }
+
+        // The data to show
+        public ProductModel Product;
+
+        /// <summary>
+        /// Default Construtor
+        /// </summary>
+        /// <param name="productService"></param>
+        public DetailUserModel(JsonFileProductService productService)
         {
+            ProductService = productService;
+        }
+
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        /// <param name="id"></param>
+        public void OnGet(string id)
+        {
+            Product = ProductService.GetProduct(id);
         }
     }
 }
