68c99e824b9c1ba4bbc1bbe506e6ff9b4d925f35
sliu1SU <sliu1@seattleu.edu>
2022-11-06 21:45:37 -0800

    removed unnecessary spacings in Startup.cs

--- src/Startup.cs
+++ src/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
 using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite
