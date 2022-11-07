3da0baeb5c6145d0875edceea77ca8092a3017b8
sliu1SU <sliu1@seattleu.edu>
2022-11-06 23:25:26 -0800

    added DetailUser to the project

---
+++ src/Pages/Restaurants/DetailUser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages.Restaurants
+{
+    public class DetailUserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
