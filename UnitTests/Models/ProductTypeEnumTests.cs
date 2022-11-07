09feef9b19eca97933786c1fe3df9c4b59b40b84
sliu1SU <sliu1@seattleu.edu>
2022-11-06 21:52:22 -0800

    removed unused using statement in ProductTypeEnumTests.cs

--- UnitTests/Models/ProductTypeEnumTests.cs
+++ UnitTests/Models/ProductTypeEnumTests.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ContosoCrafts.WebSite.Models;
-using ContosoCrafts.WebSite.Pages.Restaurants;
+﻿using ContosoCrafts.WebSite.Models;
 using NUnit.Framework;
-using static NUnit.Framework.Internal.OSPlatform;
 
 namespace UnitTests.Models
 {
