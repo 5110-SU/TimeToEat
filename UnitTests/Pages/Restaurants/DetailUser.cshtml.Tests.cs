8ab27e10a09de421b149ab2c986c8c2ea6608849
sliu1SU <sliu1@seattleu.edu>
2022-11-07 10:33:06 -0800

    removed unused using statements in DetailUser.cshtml.Tests.cs

--- UnitTests/Pages/Restaurants/DetailUser.cshtml.Tests.cs
+++ UnitTests/Pages/Restaurants/DetailUser.cshtml.Tests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using ContosoCrafts.WebSite.Pages.Restaurants;
 using NUnit.Framework;
 
