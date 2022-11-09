c6f578caa86ec93bd91214fcabeb13e1313223af
Duc Vo <dvo4@seattleu.edu>
2022-11-08 22:45:51 -0800

    Added unit test for onget method for product that has weekday null hour in Detail.cshtml.Tests.cs

--- UnitTests/Pages/Restaurants/Detail.cshtml.Tests.cs
+++ UnitTests/Pages/Restaurants/Detail.cshtml.Tests.cs
@@ -1,4 +1,5 @@
-﻿using ContosoCrafts.WebSite.Models;
+﻿using System.Collections.Generic;
+using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Pages.Restaurants;
 using NUnit.Framework;
 using System.Linq;
@@ -77,6 +78,28 @@ namespace UnitTests.Pages.Restaurants
                 {
                     Title = "Test Product",
                     Hours = null
+                }
+            );
+            
+            // Act
+            pageModel.OnGet(product.Id);
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+
+        /// <summary>
+        /// Test the OnGet Http request method product has weekday null hour
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_With_Product_That_Has_Weekday_Hour_Should_Return_Same_Page()
+        {
+            // Arrange
+            var product = TestHelper.ProductService.CreateProduct(
+                new ProductModel()
+                {
+                    Title = "Test Product",
+                    Hours = new List<int[]>() { null, null, null, null, null, null, null }
                 }
             );
             
